9e84bdc7b33c7ae5cfa25dcfbb52b91c50f0a6c1
Peter Varkoly <varkoly@suse.de>
2013-11-30 21:55:10 +0100

    Do not reload squid only if the group membership was changed

--- system/usr/share/oss/plugins/modify_user/refresh_grouping.sh
+++ system/usr/share/oss/plugins/modify_user/refresh_grouping.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Copyright (c) 2012 Peter Varkoly <peter@varkoly.de> Nürnberg, Germany.  All rights reserved.
+while read a
+do
+  if [ -z "$G" ]; then
+    G=$( echo $a | grep group )
+  fi
+done
+if [ -z "$G" ]; then
+        exit
+fi
 test -e /var/run/import_user.pid && exit
 nscd -i group
 nscd -i passwd
