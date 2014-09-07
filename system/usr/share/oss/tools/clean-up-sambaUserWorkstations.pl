ecb7eabd17887712a9a81af5be4d768fb9dcf7bf
Peter Varkoly <varkoly@suse.de>
2014-09-07 14:12:06 +0200

    Clean up all LOGGED_ON attributes if attribute --force is set.

--- system/usr/share/oss/tools/clean-up-sambaUserWorkstations.pl
+++ system/usr/share/oss/tools/clean-up-sambaUserWorkstations.pl
@@ -56,21 +56,19 @@ if( defined($options{'force'}) ){
 
 my $oss   = oss_base->new();
 
-# Read some values from rcconfig
-my $allow_multiple_login          = $oss->get_school_config("SCHOOL_ALLOW_MULTIPLE_LOGIN")          =~ /yes/i ? 1:0;
-my $allow_students_multiple_login = $oss->get_school_config("SCHOOL_ALLOW_STUDENTS_MULTIPLE_LOGIN") =~ /yes/i ? 1:0;
-
 my $smbstatus = cmd_pipe("/usr/bin/smbstatus -b","");
 
 my $LoggedOnByUID = {};
 my $LoggedOnByCN  = {};
 
-foreach( split /\n/,$smbstatus )
-{
-	if( /^\d+\s+(\S+)\s+(\S+)\s+(\S+)\s+\((\d+\.\d+\.\d+\.\d+)\)/ )
+if( !$force ) {
+	foreach( split /\n/,$smbstatus )
 	{
-		$LoggedOnByUID->{"$1-LOGGED_ON=$4"} = 1;
-		$LoggedOnByCN->{"$3-LOGGED_ON=$1"}  = 1;
+		if( /^\d+\s+(\S+)\s+(\S+)\s+(\S+)\s+\((\d+\.\d+\.\d+\.\d+)\)/ )
+		{
+			$LoggedOnByUID->{"$1-LOGGED_ON=$4"} = 1;
+			$LoggedOnByCN->{"$3-LOGGED_ON=$1"}  = 1;
+		}
 	}
 }
 #First we clean up the user accounts
@@ -97,8 +95,6 @@ foreach my $entry ( $mesg->entries ) {
 	}
 	#The sambaUserWorkstations attribut is mostly set by guest accounts.
 	next if( $oss->is_guest( $entry->dn ));
-	next if( !$force && $allow_students_multiple_login && $oss->is_student( $entry->dn )); 
-	next if( !$force && $allow_multiple_login          && !$oss->is_student( $entry->dn )); 
 	$oss->{LDAP}->modify( $entry->dn, delete => { sambaUserWorkstations => [] } );
 }
 
