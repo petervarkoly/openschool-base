c7d1d042f2e87bc5469701480be6a5bd56ad96c0
Peter Varkoly <varkoly@suse.de>
2015-10-21 16:01:05 +0200

    Do not wrap the lines.

--- scripts/add-rasaccess-index.sh
+++ scripts/add-rasaccess-index.sh
@@ -2,3 +2,9 @@ echo "dn: olcDatabase={1}hdb,cn=config
 add: olcDbIndex
 olcDbIndex: rasAccess eq" | ldapmodify -Y external -H ldapi:/// 
 
+
+echo "dn: olcDatabase={1}hdb,cn=config
+add: olcDbIndex
+olcDbIndex: o eq" | ldapmodify -Y external -H ldapi:///
+
+
