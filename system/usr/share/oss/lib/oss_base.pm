c3232e9e4a26317866f4937ec9ad147a09076cf7
Peter Varkoly <varkoly@suse.com>
2015-12-09 21:25:11 +0100

    Reset snr if the day was changed in SOA. Take care that snr must not be graeter then 99.

--- system/usr/share/oss/lib/oss_base.pm
+++ system/usr/share/oss/lib/oss_base.pm
@@ -4779,11 +4779,12 @@ sub update_soa
 
   if( $timenr eq $timenow )
   {
-    $sernr++;
+      $sernr++ if( $sernr < 100 );
   }
   else
   {
     $timenr = $timenow;
+    $sernr  = 10;
   }
 
   $zone_entry->replace( sOARecord =>
