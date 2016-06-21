273ba56b99265f43d34266e5563b595a94420f6d
Peter Varkoly <varkoly@suse.de>
2016-06-21 13:57:47 +0200

    Cephalix uses this script too. We can not put anything into the logfile.

--- system/usr/sbin/oss_update.sh
+++ system/usr/sbin/oss_update.sh
@@ -1,11 +1,7 @@
-echo 'DATE=`/usr/share/oss/tools/oss_date.sh`
+DATE=$( /usr/share/oss/tools/oss_date.sh )
 . /etc/profile.d/profile.sh
 echo "/var/log/OSS-UPDATE-$DATE" > /var/adm/oss/update-started
-zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses  &> /var/log/OSS-UPDATE-$DATE
-if [ $? ]; then 
-echo "You have to reboot your OSS-server!
-Bitte starten Sie Ihren OSS-Server neu!" > /var/adm/oss/must-restart
-fi
+zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses  2>&1 | tee /var/log/OSS-UPDATE-$DATE
 /etc/cron.daily/oss.list-updates
 rm /var/adm/oss/update-started
-' | at now
+
