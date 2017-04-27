d2f7a833a540e68330c51560a3c3ad53d5b68cf6
Peter Varkoly <varkoly@suse.com>
2017-04-27 18:35:51 +0200

    Make able to prohibit to execute the plugins to acoid endless loops.

--- system/usr/sbin/oss_del_user.pl
+++ system/usr/sbin/oss_del_user.pl
@@ -14,6 +14,12 @@ binmode STDIN, ':utf8';
 my @dns     = ();
 my $connect  = { withIMAP => 1 };
 
+while(my $param = shift)
+{
+    if( $param =~ /text/i ) { $connect->{XML}=0; }
+    if( $param =~ /--noplugin/i ) { $connect->{noplugin}=1; }
+}
+
 while(<STDIN>){
     # Clean up the line!
     chomp; s/^\s+//; s/\s+$//;
